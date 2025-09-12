71ce42ec7c2b49573df066db87d6ce874278f37a
Abdiel Lopez <48071553+PaperPrototype@users.noreply.github.com>
2025-09-12 15:38:27 -0400

    added service worker

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,11 +1,20 @@
+// Disables access to DOM typings like `HTMLElement` which are not available
+// inside a service worker and instantiates the correct globals
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
 
+// Ensures that the `$service-worker` import has proper type definitions
+/// <reference types="@sveltejs/kit" />
 
+// Only necessary if you have an import from `$env/static/public`
+/// <reference types="../.svelte-kit/ambient.d.ts" />
 
-// self.onrequest("", (request) => {
+// This gives `self` the correct types
+const serviceWorker = globalThis.self as unknown as ServiceWorkerGlobalScope;
 
-//     let rootFolder = navigation.storagge.getDIrectory();
-
-//     let file = rootFolder.getFile(request.pathname, { create: true });
-
-//     return new Response(file);
-// })
+serviceWorker.addEventListener("fetch", (event) => {
+    
+    // default behaviour: pass through
+    event.respondWith(fetch(event.request));
+})
